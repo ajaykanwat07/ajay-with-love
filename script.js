0f8e0c609ed25cdcb4d940203c89f26182b3d98a
ajaykanwat07 <ajaykanwat07@gmail.com>
2026-07-21 19:21:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -160,35 +160,24 @@ setTimeout(()=>{
 
 console.log("Welcome to Ajay With Love ❤️");
 
-},1200);
-// Gallery Lightbox
-
-const galleryImages = document.querySelectorAll(".gallery-card img");
+const galleryImages = document.querySelectorAll(".gallery-grid img");
 const lightbox = document.getElementById("lightbox");
 const lightboxImg = document.getElementById("lightbox-img");
 const closeBtn = document.getElementById("close");
 
 galleryImages.forEach(img => {
-
     img.addEventListener("click", () => {
-
         lightbox.style.display = "flex";
         lightboxImg.src = img.src;
-
     });
-
 });
 
 closeBtn.addEventListener("click", () => {
-
     lightbox.style.display = "none";
-
 });
 
 lightbox.addEventListener("click", (e) => {
-
-    if(e.target === lightbox){
+    if (e.target === lightbox) {
         lightbox.style.display = "none";
     }
-
 });
